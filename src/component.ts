bb0cec3ad22001c2dd2df32b6b891fcce6ebcbd3
Murilo Oliveira de Araujo <muritavo@outlook.com>
2021-08-14 03:34:31 -0300

    Fixes

--- src/component.ts
+++ src/component.ts
@@ -44,25 +44,15 @@ export default function createComponent() {
         l("Creating the component", componentName);
         const componentFolder = join(rootFolder, componentName);
         mkdirSync(componentFolder, { recursive: true });
-        writeFileSync(join(componentFolder, `index.tsx`), `export { default } from './${componentName}';\n`);
-        writeFileSync(join(componentFolder, `${componentName}.tsx`), `import React from 'react';
-${generationFeatures.includes('scss') ? `import Styles from './${componentName}.module.scss';\n` : ''}
-/**
- * ${componentDescription}
- **/
-export default function ${componentName}() {
-    return <>${componentName}</>;
-}`);
-        if (generationFeatures.includes('tests'))
-            writeFileSync(join(componentFolder, `${componentName}.test.tsx`), `import React from 'react';
-import { render } from '@testing-library/react';
-import Component from './${componentName}';
-
-it('Should at least render :)', () => {
-    render(<Component/>);
-})`);
-        if (generationFeatures.includes('scss'))
-            writeFileSync(join(componentFolder, `${componentName}.module.scss`), ``);
+        Object
+            .entries(generateOutput(
+                componentName,
+                componentDescription,
+                (generationFeatures as (keyof Features)[]).reduce((r, f) => ({ ...r, [f]: true }), {} as Features)
+            ))
+            .forEach(([filename, fileContent]) => {
+                writeFileSync(join(componentFolder, filename), fileContent);
+            })
     })
 }
 
